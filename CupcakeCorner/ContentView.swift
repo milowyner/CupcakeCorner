210319448aa8cc28961ef3bae583f22a8b6405e6
Milo Wyner <milowyner@gmail.com>
2021-08-16 16:47:09 -0700

    Create a network request to get a list of songs from Apple's iTunes API and show them in a list

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -7,28 +7,48 @@
 
 import SwiftUI
 
-class User: ObservableObject, Codable {
-    enum CodingKeys: CodingKey {
-        case name
-    }
-    
-    @Published var name = "Paul Hudson"
-    
-    required init(from decoder: Decoder) throws {
-        let container = try decoder.container(keyedBy: CodingKeys.self)
-        name = try container.decode(String.self, forKey: .name)
-    }
-    
-    func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(name, forKey: .name)
-    }
+struct Response: Codable {
+    var results: [Result]
+}
+
+struct Result: Codable {
+    var trackId: Int
+    var trackName: String
+    var collectionName: String
 }
 
 struct ContentView: View {
+    @State private var results = [Result]()
+    
     var body: some View {
-        Text("Hello World")
-            .padding()
+        List(results, id: \.trackId) { item in
+            VStack(alignment: .leading) {
+                Text(item.trackName)
+                    .font(.headline)
+                Text(item.collectionName)
+            }
+        }
+        .onAppear(perform: loadData)
+    }
+    
+    func loadData() {
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data,
+               let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                DispatchQueue.main.async {
+                    results = decodedResponse.results
+                }
+                return
+            } else {
+                print("Fetch failed: \(error?.localizedDescription ?? "Unknown error")")
+            }
+        }.resume()
     }
 }
 
